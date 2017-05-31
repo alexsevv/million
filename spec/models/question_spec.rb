390b5fee47f5da42ee48f5661ffad6a0e369be72
alexsevv <aaa123qqq@mail.ru>
2017-05-31 14:48:55 +0300

    rspec task 61-1

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe Question, type: :model do
     it { should allow_value(14).for(:level) }
     it { should_not allow_value(15).for(:level) }
   end
+
+  # подсказка: читать доку на метод validate_uniqueness_of, там примеры
+  # явно создаем "предмет тестирования" - валидный объект
+  subject { Question.new(text: 'some', level: 0, answer1: '1', answer2: '1', answer3: '1', answer4: '1') }
+
+  # только с валидным объектом работает этот тест,
+  # иначе пытается создать в базе новый невалидный
+  it { should validate_uniqueness_of :text }
 end
