443779f6ff484d6fe486723d31c8e906e961b457
alexsevv <aaa123qqq@mail.ru>
2017-05-31 13:57:17 +0300

    rspec models

---
+++ spec/models/game_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+# Наш собственный класс с вспомогательными методами
+require 'support/my_spec_helper'
+
+RSpec.describe Game, type: :model do
+  let(:user) { FactoryGirl.create(:user) }
+
+  let(:game_w_questions) { FactoryGirl.create(:game_with_questions, user: user) }
+
+  # Группа тестов на работу фабрики создания новых игр
+  context 'Game Factory' do
+    it 'Game.create_game! new correct game' do
+      # Генерим 60 вопросов с 4х запасом по полю level, чтобы проверить работу
+      # RANDOM при создании игры.
+      generate_questions(60)
+
+      game = nil
+
+      # Создaли игру, обернули в блок, на который накладываем проверки
+      expect {
+        game = Game.create_game_for_user!(user)
+        # Проверка: Game.count изменился на 1 (создали в базе 1 игру)
+      }.to change(Game, :count).by(1).and(
+        # GameQuestion.count +15
+        change(GameQuestion, :count).by(15).and(
+          # Game.count не должен измениться
+          change(Question, :count).by(0)
+        )
+      )
+
+      # Проверяем статус и поля
+      expect(game.user).to eq(user)
+      expect(game.status).to eq(:in_progress)
+
+      # Проверяем корректность массива игровых вопросов
+      expect(game.game_questions.size).to eq(15)
+      expect(game.game_questions.map(&:level)).to eq (0..14).to_a
+    end
+  end
+
+
+  context 'game mechanics' do
+    it 'answer correct continues' do
+      level = game_w_questions.current_level
+      q = game_w_questions.current_game_question
+      expect(game_w_questions.status).to eq(:in_progress)
+
+      game_w_questions.answer_current_question!(q.correct_answer_key)
+
+      expect(game_w_questions.current_level).to eq(level + 1)
+
+      expect(game_w_questions.current_game_question).not_to eq q
+
+      expect(game_w_questions.status).to eq(:in_progress)
+      expect(game_w_questions.finished?).to be_falsey
+    end
+  end
+end
