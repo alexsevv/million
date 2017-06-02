599695f6480dc701281e4a7f2f1094f421d3a462
alexsevv <aaa123qqq@mail.ru>
2017-06-02 09:48:42 +0300

    GamesController not create second game

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -91,5 +91,21 @@ RSpec.describe GamesController, type: :controller do
       expect(flash[:warning]).to be
     end
 
+    # юзер пытается создать новую игру, не закончив старую
+    it 'try to create second game' do
+      # убедились что есть игра в работе
+      expect(game_w_questions.finished?).to be_falsey
+
+      # отправляем запрос на создание, убеждаемся что новых Game не создалось
+      expect { post :create }.to change(Game, :count).by(0)
+
+      game = assigns(:game) # вытаскиваем из контроллера поле @game
+      expect(game).to be_nil
+
+      # и редирект на страницу старой игры
+      expect(response).to redirect_to(game_path(game_w_questions))
+      expect(flash[:alert]).to be
+    end
+
   end
 end
