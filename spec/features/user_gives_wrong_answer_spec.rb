7e4fce86758eb20021863c13de61bcd38229e553
alexsevv <aaa123qqq@mail.ru>
2017-06-22 13:44:41 +0300

    интегральный тест - юзер дает неправльные ответы

---
+++ spec/features/user_gives_wrong_answer_spec.rb
@@ -0,0 +1,36 @@
+# Как и в любом тесте, подключаем помощник rspec-rails
+require 'rails_helper'
+
+# Начинаем описывать функционал, связанный с созданием игры
+RSpec.feature 'USER gives wrong answer', type: :feature do
+
+  #создам игру с вопросами
+  let(:game_w_questions) { FactoryGirl.create(:game_with_questions) }
+
+  before(:each) do
+    login_as game_w_questions.user
+  end
+
+  scenario 'successfully' do
+    #1 неправильный ответ
+    visit game_path(Game.last)
+    click_link 'A'
+    expect(page).to have_content 'Игра закончена, ваш приз 0 ₽'
+  end
+
+  scenario 'successfully' do
+    #1 неправильный ответ после 7 правильных
+    visit game_path(Game.last)
+    7.times { click_link 'D' }
+    click_link 'A'
+    expect(page).to have_content 'Игра закончена, ваш приз 1 000 ₽'
+  end
+
+  scenario 'successfully' do
+    #1 неправильный ответ после 12 правильных
+    visit game_path(Game.last)
+    12.times { click_link 'D' }
+    click_link 'A'
+    expect(page).to have_content 'Игра закончена, ваш приз 32 000 ₽'
+  end
+end
